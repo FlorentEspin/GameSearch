70ee14a2163b0dcbfaabb111aa3221e0aada98ce
FlorentEspin <florent.espin@hotmail.fr>
2015-06-27 16:35:33 +0200

    Modify Manifest
    LOG

--- app/src/main/java/gameproject/gamesearch/CrudUtilisateur.java
+++ app/src/main/java/gameproject/gamesearch/CrudUtilisateur.java
@@ -1,10 +1,43 @@
 package gameproject.gamesearch;
 
+import android.util.Log;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
 /**
  * Created by Nox on 27/06/2015.
  */
 public class CrudUtilisateur {
 
-    public Utilisateur
+    Create request = new Create();
+    final ApiService api=   request.getApiService();
 
+    public Utilisateur getUserById(int id)
+    {
+
+    }
+
+    public void createUser(Utilisateur aUser)
+    {
+        try
+        {
+            api.createUtilisateur(aUser, new Callback<String>() {
+                @Override
+                public void success(String s, Response response) {
+                   
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+
+                }
+            });
+        }
+        catch(Exception e)
+        {
+
+        }
+    }
 }

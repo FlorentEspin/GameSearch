563a932bb64e7a361744252374570c4916cfe982
FlorentEspin <florent.espin@hotmail.fr>
2015-06-29 14:43:19 +0200

    Updated the listUser part

---
+++ app/src/main/java/gameproject/gamesearch/CRUD.java
@@ -0,0 +1,79 @@
+package gameproject.gamesearch;
+
+import android.database.Observable;
+
+import com.google.gson.Gson;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import retrofit.Callback;
+import retrofit.http.Body;
+import retrofit.http.Field;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.PUT;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
+/**
+ * Created by Nox on 15/05/2015.
+ */
+public interface CRUD {
+//region CRUD FOR GAMES
+    @GET("/api/Jeux")
+    public void getAllGames(Callback<ArrayList<Jeu>> cb);
+    @GET("/api/Jeux")
+    public void getGameById(@Query("id")int gameID, Callback<Jeu> cb);
+    @GET("/api/Jeux")
+    public void getGameByName(@Query("name")String gameName, Callback<Jeu> cb);
+    @POST("/api/jeux")
+    public void createGame(@Body Jeu unJeu, Callback<Jeu> cb);
+    @POST("/api/jeux")
+    public void UpdateGame(@Body Jeu unJeu, Callback<Jeu> cb);
+//endregion
+//region CRUD FOR USERS
+
+    @GET("/api/Utilisateurs")
+    public void getAllUser(Callback<ArrayList<Utilisateur>> cb);
+    @GET("/api/Utilisateurs")
+    public void getUserById(@Query("id")int userID, Callback<Utilisateur> cb);
+    @GET("/api/Utilisateurs")
+    public void getUserByName(@Query("name")String gameName, Callback<Utilisateur> cb);
+    @POST("/api/Utilisateurs")
+    public void createUtilisateur(@Body Utilisateur aUser, Callback<Utilisateur> cb);
+    @POST("/api/Utilisateurs")
+    public void updateUser(@Body Utilisateur aUser, Callback<Utilisateur> cb);
+    //endregion
+//region CRUD FOR EDITORS
+
+    @GET("/api/Editeurs")
+    public void getAllEditor(Callback<ArrayList<Editeur>> cb);
+    @GET("/api/Editeurs")
+    public void getEditorById(@Query("id")int editorID, Callback<Editeur> cb);
+    @GET("/api/Editeurs")
+    public void getEditorByName(@Query("name")String editorName, Callback<Editeur> cb);
+    @POST("/api/Editeurs")
+    public void createEditor(@Body Utilisateur aEditor, Callback<Editeur> cb);
+    @POST("/api/Editeurs")
+    public void updateEditor(@Body Utilisateur aEditor, Callback<Editeur> cb);
+    //endregion
+
+//region CRUD FOR KINDS
+
+    @GET("/api/Utilisateurs")
+    public void getAllKind(Callback<ArrayList<Genre>> cb);
+    @GET("/api/Utilisateurs")
+    public void getKindById(@Query("id")int kindID, Callback<Genre> cb);
+    @GET("/api/Utilisateurs")
+    public void getKindByName(@Query("name")String kindName, Callback<Genre> cb);
+    @POST("/api/Utilisateurs")
+    public void createKind(@Body Genre aKind, Callback<Genre> cb);
+    @POST("/api/Utilisateurs")
+    public void updateKind(@Body Genre aKind, Callback<Genre> cb);
+    //endregion
+}
